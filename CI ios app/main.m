27fb5103b2bb252e8bbf4c853e82eeacba105031
Filip Jandrijević <filipjandrijevic@Filips-MacBook-Air.local>
2015-05-13 10:12:04 +0200

    Initial Commit

---
+++ CI ios app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CI ios app
+//
+//  Created by Filip Jandrijević on 13/05/15.
+//  Copyright (c) 2015 Filip Jandrijević. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
